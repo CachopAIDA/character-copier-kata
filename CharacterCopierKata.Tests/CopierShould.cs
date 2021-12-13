c497853a855c5288dde30c1ed90f587df14f42b0
Santi Diaz <sdiaz@aidacanarias.com>
2021-12-13 15:15:53 +0000

    Test for copy

--- CharacterCopierKata.Tests/CopierShould.cs
+++ CharacterCopierKata.Tests/CopierShould.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Xunit;
 
 namespace CharacterCopierKata.Tests
@@ -5,9 +6,24 @@ namespace CharacterCopierKata.Tests
     public class CopierShould
     {
         [Fact]
-        public void DoSomething()
+        public void When_Copy_then_destination_is_equal_source()
         {
-            // TODO: Implement
+            const string content = "asasdas";
+            SourceFake source = new SourceFake(content);
+            DestinationFake destination = new DestinationFake();
+            Assert.Equal(destination.Content, source.Content);
         }
+    }
+    public class SourceFake : ISource
+    {
+        public SourceFake(string content)
+        {
+            throw new System.NotImplementedException();
+        }
+        public string Content { get; set; }
+    }
+    public class DestinationFake : IDestination
+    {
+        public string Content { get; set; }
     }
 }
