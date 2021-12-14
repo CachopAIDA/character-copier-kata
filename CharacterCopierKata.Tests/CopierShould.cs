4d7640381a2af4dea02cdca036e4ca387a44e48f
Claudio López Ardura <cllopez@aidacanarias.com>
2021-12-14 16:56:48 +0100

    Added test for differents string sizes and refactors

--- CharacterCopierKata.Tests/CopierShould.cs
+++ CharacterCopierKata.Tests/CopierShould.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using Moq;
 using Xunit;
 
@@ -5,32 +6,52 @@ namespace CharacterCopierKata.Tests
 {
     public class CopierShould
     {
-        [Fact]
-        public void copy_content_from_source_and_destination_with_1_char()
+
+        private readonly ISource source;
+        private readonly IDestination destination;
+
+        public CopierShould()
         {
-            var source = Mock.Of<ISource>();
-            var destination = Mock.Of<IDestination>();
-            Mock.Get(source).Setup(s => s.GetContent()).Returns("a");
+            this.source = Mock.Of<ISource>();
+            this.destination = Mock.Of<IDestination>();
+        }
+
+        private void CallCopy(string stringToCopy)
+        {
+            Mock.Get(source).Setup(s => s.GetContent()).Returns(stringToCopy);
 
             var copier = new Copier(source, destination);
             copier.Copy();
+        }
 
-            Mock.Get(destination).Verify(d => d.SetContent(It.IsAny<string>()), Times.Once);
+        [Theory]
+        [InlineData("a", 1)]
+        [InlineData("ab", 2)]
+        public void copy_content_from_source_and_destination_with_1_char(string stringToCopy, int destinationTimes)
+        {
+            CallCopy(stringToCopy);
+
+            Mock.Get(destination).Verify(d => d.SetContent(It.IsAny<string>()), Times.Exactly(destinationTimes));
             Mock.Get(source).Verify(s => s.GetContent(), Times.Once);
         }
 
         [Fact]
-        public void copy_from_source_and_destination_with_2_char()
+        public void copy_the_characters_of_asdf_in_destination()
         {
-            var source = Mock.Of<ISource>();
-            var destination = Mock.Of<IDestination>();
-            Mock.Get(source).Setup(s => s.GetContent()).Returns("ab");
+            CallCopy("asdf");
 
-            var copier = new Copier(source, destination);
-            copier.Copy();
+            Mock.Get(destination).Verify(d => d.SetContent(It.Is<string>(s=> s == "a")), Times.Once());
+            Mock.Get(destination).Verify(d => d.SetContent(It.Is<string>(s => s == "s")), Times.Once());
+            Mock.Get(destination).Verify(d => d.SetContent(It.Is<string>(s => s == "d")), Times.Once());
+            Mock.Get(destination).Verify(d => d.SetContent(It.Is<string>(s => s == "f")), Times.Once());
+        }
 
-            Mock.Get(destination).Verify(d => d.SetContent(It.IsAny<string>()), Times.Exactly(2));
-            Mock.Get(source).Verify(s => s.GetContent(), Times.Once);
+        [Fact]
+        public void when_source_is_empty_destination_is_not_called()
+        {
+            CallCopy("");
+
+            Mock.Get(destination).Verify(d => d.SetContent(It.IsAny<string>()), Times.Never);
         }
     }
 }
